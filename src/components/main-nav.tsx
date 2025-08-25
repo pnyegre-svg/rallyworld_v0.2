37e3246c17fd16241dc9e3aa86f941b2b7064609
Ion Nyegre <pnyegre@gmail.com>
2025-08-25 23:58:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -49,15 +49,16 @@ export function MainNav() {
             (item) =>
               item.roles.includes(user.role) && (
                 <SidebarMenuItem key={item.href}>
-                  <Link href={item.href} legacyBehavior passHref>
-                    <SidebarMenuButton
-                      isActive={pathname === item.href}
-                      tooltip={item.label}
-                    >
+                  <SidebarMenuButton
+                    asChild
+                    isActive={pathname === item.href}
+                    tooltip={item.label}
+                  >
+                    <Link href={item.href}>
                       <item.icon />
                       <span>{item.label}</span>
-                    </SidebarMenuButton>
-                  </Link>
+                    </Link>
+                  </SidebarMenuButton>
                 </SidebarMenuItem>
               )
           )}
