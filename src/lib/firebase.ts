f8fc15bfd3e67a4e2533109d825a4260da756cfc
Ion Nyegre <pnyegre@gmail.com>
2025-08-26 01:00:36 +0000

    can we move from the localhost? try something different?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   appId: '1:67027100425:web:832f96f8228d9346e4a9d8',
   storageBucket: 'rally-world.firebasestorage.app',
   apiKey: 'AIzaSyD1SDoZo2M84Lqy6w6zbK3ZhWKtQcGQf-g',
-  authDomain: 'rally-world.firebaseapp.com',
+  authDomain: '6000-firebase-studio-1756165267754.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev',
   measurementId: '',
   messagingSenderId: '67027100425',
 };
