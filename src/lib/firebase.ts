8acb83c8e0e19fec2bb3efe56fdad7f5c53bc104
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 18:57:05 +0000

    which gs:// is the app submitting to?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   projectId: 'rally-world',
   appId: '1:67027100425:web:832f96f8228d9346e4a9d8',
-  storageBucket: 'rally-world',
+  storageBucket: 'rally-world.firebaseapp.com',
   apiKey: 'AIzaSyD1SDoZo2M84Lqy6w6zbK3ZhWKtQcGQf-g',
   authDomain: 'rally-world.firebaseapp.com',
   measurementId: '',
