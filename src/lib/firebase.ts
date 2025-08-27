900ef920c8b7c88a99efbf8249f132ee888891b7
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 07:25:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
   messagingSenderId: '67027100425',
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
