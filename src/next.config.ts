eada442d906d6d1e80b04a26ea02180f0ef039df
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 21:40:35 +0000

    Invalid next.config.ts options detected:  ⚠     Unrecognized key(s) in

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+  },
+  allowedDevOrigins: [
+    'https://9003-firebase-studio-1756165267754.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev',
+  ],
+};
+
+export default nextConfig;
