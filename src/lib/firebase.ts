b84b7469171e6291abab48fa6a856f15fa739154
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 20:04:28 +0000

    i don't have a recaptcha v3 key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,9 +20,11 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize App Check on the client
 if (typeof window !== 'undefined') {
-  initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!),
-    isTokenAutoRefreshEnabled: true,
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider('6Ld6gBEqAAAAAJe9Xlz2-L-4A0T4_2uL4Cq2O5_G'),
+    isTokenAutoRefreshEnabled: true
   });
 }
 
