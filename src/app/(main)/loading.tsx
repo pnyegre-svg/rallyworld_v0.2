2f723c1d07d6b526add7ffd01638901bf317ed46
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 23:45:55 +0000

    I noticed this is the loading page, Can you change the "i" with my image

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -1,17 +1,15 @@
-import { Skeleton } from '@/components/ui/skeleton';
+import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="space-y-6">
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-32 w-full" />
-      </div>
-      <div className="space-y-4">
-        <Skeleton className="h-12 w-full" />
-        <Skeleton className="h-48 w-full" />
-      </div>
+    <div className="flex h-full w-full items-center justify-center">
+      <Image
+        src="/RW_i.svg"
+        alt="Rally World Logo"
+        width={80}
+        height={80}
+        className="animate-pulse"
+      />
     </div>
   );
 }
