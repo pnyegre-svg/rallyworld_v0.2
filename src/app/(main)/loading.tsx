b132b041e6c984a3a27c7176c84b6c2a32446718
Ion Nyegre <pnyegre@gmail.com>
2025-08-27 23:48:06 +0000

    I've placed my picture in the /public folder, it is called: RW_transp.sv

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <Image
-        src="/RW_i.svg"
+        src="/RW_transp.svg"
         alt="Rally World Logo"
         width={80}
         height={80}
