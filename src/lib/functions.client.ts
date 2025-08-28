4104fdff1cd9fcb3987193aaf7c25eaaf7d02fd3
Ion Nyegre <pnyegre@gmail.com>
2025-08-28 13:27:33 +0000

    Client wiring to Functions
    src/lib/functions.client.ts
    'use client';
    im

--- src/lib/functions.client.ts
+++ src/lib/functions.client.ts
@@ -1,15 +1,6 @@
-
 'use client';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 import { app } from '@/lib/firebase.client';
-
-
 const fns = getFunctions(app);
-
-
-export async function approveEntryFn(eventId: string, entryId: string) {
-return await httpsCallable(fns, 'approveEntry')({ eventId, entryId });
-}
-export async function markEntryPaidFn(eventId: string, entryId: string) {
-return await httpsCallable(fns, 'markEntryPaid')({ eventId, entryId });
-}
+export async function approveEntryFn(eventId: string, entryId: string){ return (await httpsCallable(fns,'approveEntry')({ eventId, entryId })).data; }
+export async function markEntryPaidFn(eventId: string, entryId: string){ return (await httpsCallable(fns,'markEntryPaid')({ eventId, entryId })).data; }
