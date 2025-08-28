a163316622ed3e71e1ce28274a583a2aed970720
Ion Nyegre <pnyegre@gmail.com>
2025-08-28 10:13:55 +0000

    Console Error
    Error: Cannot read properties of undefined (reading 'get

--- src/lib/app-check.ts
+++ src/lib/app-check.ts
@@ -2,11 +2,13 @@
 'use client';
 
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
-import { app } from './firebase';
+import { getFirebaseApp } from './firebase';
+
 let inited = false;
-export function initAppCheck(){
-if (inited || typeof window==='undefined') return;
-(window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.NEXT_PUBLIC_FB_APPCHECK_DEBUG_TOKEN;
-initializeAppCheck(app,{ provider:new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_FB_APPCHECK_SITE_KEY!), isTokenAutoRefreshEnabled:true});
-inited = true;
+export function initAppCheck() {
+    if (inited || typeof window === 'undefined') return;
+    const app = getFirebaseApp(); // Get the initialized app instance
+    (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.NEXT_PUBLIC_FB_APPCHECK_DEBUG_TOKEN;
+    initializeAppCheck(app, { provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_FB_APPCHECK_SITE_KEY!), isTokenAutoRefreshEnabled: true });
+    inited = true;
 }
